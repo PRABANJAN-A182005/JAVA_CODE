6a4942a3643bfdf5fe3a6fd0d589470a0e65e023
PRABANJAN A <prabanjan.offical@gmail.com>
2025-01-27 22:04:59 +0530

    Add files via upload

---
+++ Basic Programs/LEAP YEAR/leapyear.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class leapyear {
+    public void leapcheck(int year){
+        
+        if((year%4==0) && (year%400==0)){
+            System.out.println(year+" IS LEAP YEAR");
+        }
+        else{
+            System.out.println(year+" IS NOT LEAP YEAR"); 
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         leapyear obj = new leapyear();
+         System.out.println("ENTER THE YEAR :- ");
+         int year = sc.nextInt();
+         obj.leapcheck(year);
+    }
+}
