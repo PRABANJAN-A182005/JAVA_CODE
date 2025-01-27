33db00a1187418472a319728ebfa20d3c948b93a
PRABANJAN A <prabanjan.offical@gmail.com>
2025-01-27 22:06:49 +0530

    Update leapyear.java
    added comment

--- Basic Programs/LEAP YEAR/leapyear.java
+++ Basic Programs/LEAP YEAR/leapyear.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class leapyear {
     public void leapcheck(int year){
-        
+        //condition for leap year to check normal and century year.
         if((year%4==0) && (year%400==0)){
             System.out.println(year+" IS LEAP YEAR");
         }
@@ -9,6 +9,7 @@ public class leapyear {
             System.out.println(year+" IS NOT LEAP YEAR"); 
         }
     }
+    //main method
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
          leapyear obj = new leapyear();
