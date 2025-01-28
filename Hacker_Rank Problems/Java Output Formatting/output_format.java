f5088f9d00ca7e3ddfecb02c37c6929865aa400b
PRABANJAN A <prabanjan.offical@gmail.com>
2025-01-28 08:05:52 +0530

    Update output_format.java
    comment defined

--- Hacker_Rank Problems/Java Output Formatting/output_format.java
+++ Hacker_Rank Problems/Java Output Formatting/output_format.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-//defined package and class
+
 public class output_format {
     //Main function is defined
     public static void main(String[] args) {
@@ -9,12 +9,12 @@ public class output_format {
             for(int i=0;i<3;i++){
                 String s1=sc.next();
                 int x=sc.nextInt();
-                //length determine by buildIn (function)method
+                
                 int lenght=s1.length();
-                //Complete this line using for loop and logic
+              
                 for(int j=lenght;j<=15;j++){
-                if(j!=15){// using if for check the string size less than 15char
-                    s1+=" ";//then increment with emptspace
+                if(j!=15){
+                    s1+=" ";
                 }
                 }
                 //integer format change to string because to print the integerexpressed in exactly digits; if the original input has less than three digits, pad output leading digits with zeroes. 
