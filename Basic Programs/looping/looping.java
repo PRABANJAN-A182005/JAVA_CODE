adeb132bdcd98bfb1847ecf93e0c433683c341ab
PRABANJAN A <prabanjan.offical@gmail.com>
2025-01-30 23:22:02 +0530

    Add files via upload

---
+++ Basic Programs/looping/looping.java
@@ -0,0 +1,8 @@
+public class looping {
+    public static void main(String[] args) {
+            for(int i=0;i<=20;i++){
+                System.out.println("ALL IS WELL");
+            }
+        
+    }
+}
