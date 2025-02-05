3d7a8c7c665c66185922a9bb8524fbfb0a653444
PRABANJAN A <prabanjan.offical@gmail.com>
2025-02-05 20:51:54 +0530

    Update perfectnum.java

--- program/perfectnum.java
+++ program/perfectnum.java
@@ -1,13 +1,15 @@
 public class perfectnum {
     public static boolean condition(int nums){
+        //logic for to find factor
         int count=0;
         int factor=0;
-        for(int i=1;i<nums;i++){
+        for(int i=1;i<nums;i++){//increment for count and sum for factor
             if(nums%i==0){
                 count++;
                 factor+=i;
             }
         }
+        //check the factor
         if(count!=2 && factor==nums){
             return true;
         }
