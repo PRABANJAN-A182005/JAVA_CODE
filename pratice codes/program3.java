afbda4b054c9d77ad97fcf7801e7cf2964c38cba
PRABANJAN-A182005 <prabanjan.offical@gmail.com>
2025-02-13 21:13:15 +0530

    added

---
+++ pratice codes/program3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class program3 {
+    public static int findNthNumberInSeries(int a, int b, int c, int N) {
+        int[] differences = { b - a, c - b };
+        int curr = c;
+        int i = 3;
+
+        while (i < N) {
+            curr += differences[(i - 3) % 2];
+            i++;
+        }
+
+        return curr;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();   
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int N = sc.nextInt();
+        System.out.println(findNthNumberInSeries(a, b, c, N));
+        sc.close();
+    }
+
+}
